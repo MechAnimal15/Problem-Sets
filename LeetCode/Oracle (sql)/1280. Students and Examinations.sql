aff41eadd669adf07826b574b97b70ba66183b94
Jonathan Martinez <49621678+MechAnimal15@users.noreply.github.com>
2024-05-05 13:28:09 -0400

    Create 1280. Students and Examinations.sql
    Create 1280. Students and Examinations.sql. 5/5/2024. 1:27 PM.
    
    Create 1280. Students and Examinations.sql
    
    
    File creation (.sql), with Oracel sql solution.

---
+++ LeetCode/Oracle (sql)/1280. Students and Examinations.sql
@@ -0,0 +1,17 @@
+''' Orcale'''
+  '''sql'''
+  ```
+    /* Write your PL/SQL query statement below */
+  SELECT s.student_id,
+         s.student_name,
+         sub.subject_name,
+         NVL(COUNT(e.subject_name), 0) AS attended_exams
+    FROM Students s
+         CROSS JOIN Subjects sub
+         LEFT JOIN Examinations e
+         ON s.student_id = e.student_id
+        AND sub.subject_name = e.subject_name
+   GROUP BY s.student_id, s.student_name, sub.subject_name
+   ORDER BY s.student_id, sub.subject_name;
+  ```
+
