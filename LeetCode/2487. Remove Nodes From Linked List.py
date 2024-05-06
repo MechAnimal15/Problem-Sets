ebc706c54fb3fcf71aec0e40e8f43bb2c4aab7fb
Jonathan Martinez <49621678+MechAnimal15@users.noreply.github.com>
2024-05-06 13:39:11 -0400

    Update 2487. Remove Nodes From Linked List.py
    Update 2487. Remove Nodes From Linked List.py. 5/6/2024. 1:38 PM.
    
    Update 2487. Remove Nodes From Linked List.py
    
    
    Updated annotated solution.

--- LeetCode/2487. Remove Nodes From Linked List.py
+++ LeetCode/2487. Remove Nodes From Linked List.py
@@ -1,37 +1,81 @@
+2487. Remove Nodes From Linked List
+[Problem Link](https://leetcode.com/problems/remove-nodes-from-linked-list/submissions/1251068805?envType=daily-question&envId=2024-05-06)
+
+
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        """
+        Initializes a node in a singly-linked list.
+        
+        Args:
+            val (int): The value of the node.
+            next (ListNode): Reference to the next node in the list.
+        """
+        self.val = val
+        self.next = next
+
 class Solution:
     def reverseLinkedList(self, head: ListNode) -> ListNode:
-        prev = None
-        current = head
-        while current:
-            next_node = current.next
-            current.next = prev
-            prev = current
-            current = next_node
-        return prev
+        """
+        Reverses a linked list in place.
+        
+        Args:
+            head (ListNode): The head of the linked list to be reversed.
+        
+        Returns:
+            ListNode: The head of the reversed linked list.
+        """
+        prev_node = None
+        current_node = head
+        
+        # Traverse the linked list and reverse the pointers
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev_node
+            prev_node = current_node
+            current_node = next_node
+        
+        return prev_node
 
     def removeNodes(self, head: ListNode) -> ListNode:
-        # Reverse the linked list
+        """
+        Removes nodes from the linked list which have a greater value anywhere to the right side of them.
+        
+        Args:
+            head (ListNode): The head of the original linked list.
+        
+        Returns:
+            ListNode: The head of the modified linked list.
+        """
+        # Reverse the linked list to simplify node removal
         head = self.reverseLinkedList(head)
         
         max_value = float('-inf')
         dummy = ListNode(0)
         dummy.next = head
-        current = head
+        current_node = head
         
-        # Iterate through the linked list
-        while current:
-            # Update the maximum value
-            max_value = max(max_value, current.val)
+        # Iterate through the linked list to find nodes to remove
+        while current_node:
+            # Update the maximum value encountered so far
+            max_value = max(max_value, current_node.val)
             
             # If the value of the current node is less than the maximum value,
-            # keep the current node in the list by updating the next pointer of the previous node
-            if current.val < max_value:
-                dummy.next = current.next
+            # remove the node from the list by updating the next pointer of the previous node
+            if current_node.val < max_value:
+                dummy.next = current_node.next
             else:
-                dummy = current
-            current = current.next
+                dummy = current_node
+            current_node = current_node.next
         
         # Reverse the linked list again to restore its original order
         head = self.reverseLinkedList(head)
         
         return head
+# ''''
+# Runtime: 458 ms
+# Memory: 50.86
+
